37515aaee43af8dce810bbff2d8eead582fc0bcc
Kay <khurrammohammed1990@gmail.com>
2019-10-23 17:26:50 +0100

    updated with working api request

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 const request = require('request');
+const url = "https://api.darksky.net/forecast/987fdd66a3ad32bfe903507e4776c423/37.8267,-122.4233"
+request({url: url }, (error, response) => {
+    const data = JSON.parse(response.body);
+    console.log(data.currently);
+})
