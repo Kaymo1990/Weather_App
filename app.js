c17733c413fcb2711f35cc405be00fd970445bd6
Kay <khurrammohammed1990@gmail.com>
2019-10-23 17:50:49 +0100

    updated api request to return weather summary

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const request = require('request');
-const url = "https://api.darksky.net/forecast/987fdd66a3ad32bfe903507e4776c423/37.8267,-122.4233"
+const url = "https://api.darksky.net/forecast/987fdd66a3ad32bfe903507e4776c423/37.8267,-122.4233?units=si"
 request({url: url, json: true }, (error, response) => {
     const data = response.body;
     // console.log(data.currently);
-    console.log(`It is current ${data.currently.temperature} degrees out.
+    console.log(`${data.daily.data[0].summary} It is current ${data.currently.temperature} degrees out.
     There is a ${data.currently.precipProbability}% chance of rain.`);
 })
