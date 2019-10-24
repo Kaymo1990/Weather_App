fabc56648f6e1b6cab6941202eafaa7379432898
Kay <khurrammohammed1990@gmail.com>
2019-10-24 23:25:39 +0100

    added latitude and longitude to geocoder

--- geocoder.js
+++ geocoder.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia2F5bW8xOTkwIiwiYSI6ImNrMjU3NGdicTJnZGYzZHF0ZGJ5aGZmb2IifQ.vT2yM-WkvkGVGNWi2SM8Gw"
 request({url: url, json: true }, (error, response) => {
-    const data = response.body;
-    console.log(data[2]);
+    const latitutde = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0]
+    console.log(latitutde);
+    console.log(longitude);
 })
