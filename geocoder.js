2789eb3b38d759e752a57abf2d74eb9a1631ac8b
Kay <khurrammohammed1990@gmail.com>
2019-10-27 22:00:12 +0000

    updating error handling

--- geocoder.js
+++ geocoder.js
@@ -2,7 +2,10 @@ const request = require('request');
 const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia2F5bW8xOTkwIiwiYSI6ImNrMjU3NGdicTJnZGYzZHF0ZGJ5aGZmb2IifQ.vT2yM-WkvkGVGNWi2SM8Gw"
 request({url: url, json: true }, (error, response) => {
     if (error) {
-        console.log("Unable to connect to location services");
+        console.log("Unable to connect to location services.");
+    }
+    else if (response.body.features.length === 0) {
+        console.log("Unable to find location.")
     }
     else {
         const latitutde = response.body.features[0].center[1];
