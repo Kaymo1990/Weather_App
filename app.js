11c6fd0abb756124667245c8ee2fa616b1153fb1
Kay <khurrammohammed1990@gmail.com>
2019-10-28 20:41:26 +0000

    fully working geocoder function with searching

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { request } from "http"
+// import { request } from "http"
 
 // const request = require('request');
 // const url = "https://api.darksky.net/forecast/987fdd66a3ad32bfe903507e4776c423/37.8267,-122.4233?units=si"
@@ -8,6 +8,7 @@ import { request } from "http"
 //     console.log(`${data.daily.data[0].summary} It is current ${data.currently.temperature} degrees out.
 //     There is a ${data.currently.precipProbability}% chance of rain.`);
 // })
+const request = require('request');
 
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1Ijoia2F5bW8xOTkwIiwiYSI6ImNrMjU3NGdicTJnZGYzZHF0ZGJ5aGZmb2IifQ.vT2yM-WkvkGVGNWi2SM8Gw'
@@ -16,7 +17,21 @@ const geocode = (address, callback) => {
         if (error) {
             callback('Unable to connect to location services.')
         }
+
+        else if (response.body.features.length === 0) {
+            callback('Unable to find location.')
+        }
+
+        else {
+            callback(undefined, {
+                latitutde: response.body.features[0].center[1],
+                longitude: response.body.features[0].center[0],
+                location: response.body.features[0].place_name
+            })
+        }
     })
 }
 
-geocode('London', (error, data) => {})
+geocode('London', (error, data) => {
+    console.log(data)
+})
